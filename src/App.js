863d12da7feb76430bf43f0e4bf925623855f334
kaso <kaso@desyn.io>
2024-08-23 22:22:34 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
 function App() {
+
+  const [tgid, setTgid] = useState(null);
+
+  useEffect(() => {
+    const tg = window.Telegram.WebApp;
+    const userId = tg.initDataUnsafe.user.id;
+    setTgid(userId);
+    
+  }, [window.Telegram]);
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {tgid ? `Your Telegram ID is ${tgid}` : 'Loading...'}
         </p>
         <a
           className="App-link"
